56c77cd24e27eab589f9756ce0072e3eeb4f0ed2
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-21 11:32:25 +0200

    started calc error

--- code/calc_error.m
+++ code/calc_error.m
@@ -1,6 +1,10 @@
 function [ error ] = calc_error( x, y, w )
-
-    error = 0;
-
+    mean_square_error = sum((y - predict(x, w)).^2) / size(x);
+    root_mean_square_error = sum(abs(y - predict(x, w))) / size(x);
+    
+    error = mean_square_error;
 end
 
+function [ real ] = predict(x,w)
+    real = x * w;
+end
