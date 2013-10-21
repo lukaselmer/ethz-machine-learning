65bd193412dc7fb00021996ff514f5814f384289
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-10-21 09:46:36 +0200

    read data / cross validation

--- code/linear_regression.m
+++ code/linear_regression.m
@@ -1,1 +1,35 @@
+% read data
+M = csvread('handout/training.csv');
 
+% split data in features / labels
+y = M(:,15);
+x = M(:,1:14);
+
+[num_data, num_features] = size(x);
+
+
+% cross validation
+K = 5;
+indices = crossvalind('Kfold', num_data, K);
+
+for i = 1:K
+    % get indices
+    test_idx = (indices == i);
+    train_idx = ~test_idx;
+    
+    % get training data
+    x_train = x(train_idx,:);
+    y_train = y(train_idx,:);
+    
+    % train
+    
+    
+    
+    
+    % get test data
+    x_test = x(test_idx,:);
+    y_test = y(test_idx,:);
+    
+    % test
+    
+end
