b6bd1b1281be80fc3e9193659a5498efcf95e636
Andreas Nufer <andreas.nufer@gridsoft.ch>
2013-10-28 11:31:06 +0100

    * added calc_cost function * added normalization * added error calculation to linear_regression.m to compare the result

--- code/linear_regression.m
+++ code/linear_regression.m
@@ -9,8 +9,8 @@ y = M(:,15);
 x = add_features(x);
 
 % normalize data
-x = normalize(x);
-y = normalize(y);
+[x, ~, ~] = normalize(x);
+[y, y_mean, y_std] = normalize(y);
 
 % add column with ones (for offset)
 x = [ones(size(x,1),1),x];
@@ -29,6 +29,14 @@ best_parameter = parameters(min_idx);
 % train with best parameter with all training data
 w = train(x,y,best_parameter);
 
+%calculate error
+y_cmp = M(:,15); % y values to calcuate the prediction error
+y_pred = (w'*x')';
+y_pred = (y_pred.*y_std) + y_mean';
+
+y_avg = mean(y_cmp);
+cv_rsme = sqrt(sum((y_pred - y_cmp).^2) / size(y_cmp, 1)) / y_avg;
+cv_rsme
 
 % generate output
 generate_output (w);
--- code/normalize.m
+++ code/normalize.m
@@ -1,4 +1,7 @@
-function [ d ] = normalize( d )
-
+function [ d, d_mean, d_std ] = normalize( d )
+    d_mean = mean(d);
+    d = d - repmat(d_mean, size(d,1), 1);
+    d_std = std(d);
+    d = d./ repmat(d_std, size(d,1), 1);
 end
 
