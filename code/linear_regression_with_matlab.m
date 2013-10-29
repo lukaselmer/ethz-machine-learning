3f914575b062dd36fc5cdb9037010dad4c4847db
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-29 22:22:05 +0100

    experimental

--- code/linear_regression_with_matlab.m
+++ code/linear_regression_with_matlab.m
@@ -20,28 +20,37 @@ t = RegressionTree.template('Surrogate','off');
 
 % rens = fitensemble(Training_X, Training_Y, 'LSBoost', 1000, t, ...
 
-strategy = 'LSBoost';
+strategy = 'Bag';
 
 if strcmp(strategy, 'Bag')
-    rens = fitensemble(Training_X, Training_Y, 'Bag', 100, 'Tree', ... %, t
+    rens_raw = fitensemble(Training_X, Training_Y, 'Bag', 50, 'Tree', ... %, t
         'PredictorNames', Training.Properties.VarNames(1:end-1), ...
-        'ResponseName', Training.Properties.VarNames{end}, ...   %'kfold', 10, ...
+        'ResponseName', Training.Properties.VarNames{end}, ..., %        'kfold', 10, ...
         'Type', 'regression')
 else
-    rens = fitensemble(Training_X, Training_Y, 'LSBoost', 100, 'Tree', ... %, t
+    rens_raw = fitensemble(Training_X, Training_Y, 'LSBoost', 100, 'Tree', ... %, t
         'PredictorNames', Training.Properties.VarNames(1:end-1), ...
         'LearnRate', 0.2, ...
         'ResponseName', Training.Properties.VarNames{end}, ...   %'kfold', 10, ...
         'Type', 'regression')
 end
 
-% 
+%% Error
+
 %    'kfold', '10', ...
 %'crossval', 'on', ...
 %'CategoricalPredictors', [3],...
+%cvrens = crossval(rens);
+
+
+rens = regularize(rens_raw,'lambda',[0.001 0.1]);
 
 Training_X = double(Training(:,1:end-1));
 Training_Prediction = predict(rens,Training_X);
+%Training_Prediction = predict(rens,Training_X);
+
+%cvrens.kfoldPredict(Training_X)
+
 y_avg = mean(Training_Y);
 cv_rsme = sqrt(sum((Training_Prediction - Training_Y).^2) / size(Training_Y, 1)) / y_avg;
 cv_rsme
@@ -51,8 +60,8 @@ cv_rsme
 Validation_X = double(Validation(:,1:end));
 Testing_X = double(Testing(:,1:end));
 
-Validation_Prediction = predict(rens,Validation_X);
-Testing_Prediction = predict(rens,Testing_X);
+Validation_Prediction = predict(rens_raw,Validation_X);
+Testing_Prediction = predict(rens_raw,Testing_X);
 
 csvwrite ('../data/testing.experimental.out', Validation_Prediction);
 csvwrite ('../data/validation.experimental.out', Validation_Prediction);
