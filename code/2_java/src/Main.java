becebf81b82594418168fcf316edc11a03d3592d
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-17 17:08:33 +0100

    java mahout scaffold

---
+++ code/2_java/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.mahout.cf.taste.model.DataModel;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// InputData = new InputData("data/2/validation.csv");
+
+		File file = new File("../../data/2/validation.csv");
+		System.out.println(file.exists());
+
+		DataModel data;
+
+	}
+
+}
