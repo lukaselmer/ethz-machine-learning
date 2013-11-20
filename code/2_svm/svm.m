fbad7b5aaeaee6f006a08c89d93df2d158335c83
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-11-20 11:29:16 +0100

    refactoring

--- code/2_svm/svm.m
+++ code/2_svm/svm.m
@@ -3,39 +3,41 @@
 training_data = csvread('../../data/2/training.csv');
 [m, ~] = size(training_data);
 
-% split data in train / test data
-% M_train = training_data(1:1200,:);
-% M_test  = training_data(1201:1447,:);
-train_data       = training_data;
-train_test_data  = training_data;
-
 % split data in features / labels
-x_train = train_data(:,1:27);
-y_train = train_data(:,28);
-
-x_train_test = train_test_data(:,1:27);
-y_train_test = train_test_data(:,28);
+x_train = training_data(:,1:27);
+y_train = training_data(:,28);
 
 %% cross validation
 %% random cross validation
 out_error = 0.12;
 min_error = 0.110000;
-best_sigma = 0.556201641;
-best_c = 1.316157273;
+best_sigma = 0.539343;
+best_c = 1.162347;
+
 
 % error: 0.110000 sigma: 0.741561491 c: 9.820265117
-% error: 0.112610 sigma: 0.539343    c: 1.162347
+% valid: 0.15272978576364893 error: 0.112610 sigma: 0.539343    c: 1.162347
 % error: 0.116188 sigma: 0.809714762 c: 5.609376671
 % error: 0.116830 sigma: 0.713188289 c: 21.681477620
 % error: 0.117462 sigma: 0.837775254 c: 94.774819336
 % error: 0.117510 sigma: 0.534608    c: 1.833623
 % error: 0.117548 sigma: 0.660176016 c: 200.472571424
-% error: 0.118166 sigma: 0.556201641 c: 1.316157273
+% valid: 0.15134761575673808 error: 0.118166 sigma: 0.556201641 c: 1.316157273
 % error: 0.118190 sigma: 0.548186    c: 1.070160
 % error: 0.118760 sigma: 0.792902199 c: 256.804165103
 % error: 0.118942 sigma: 0.665753253 c: 57.866211899
 % error: 0.119550 sigma: 0.528402    c: 1.594627
 % error: 0.119559 sigma: 0.672394589 c: 5.477502294
+% 
+% error: 0.119521 sigma: 0.591590830 c: 1.224117540
+% error: 0.119555 sigma: 0.560523922 c: 1.390024599
+% error: 0.118855 sigma: 0.561340400 c: 1.386480247
+% error: 0.116724 sigma: 0.542125355 c: 1.124139940
+% error: 0.116839 sigma: 0.524691191 c: 1.180194827
+% error: 0.118812 sigma: 0.554472701 c: 1.260923921
+% error: 0.116786 sigma: 0.533973557 c: 1.328833169
+% error: 0.116078 sigma: 0.546222230 c: 1.236622461
+% valid: 0.14858327574291638 error: 0.110651 sigma: 0.562775896 c: 1.058211034
 
 for i = 1:0  
     
@@ -46,7 +48,7 @@ for i = 1:0
     %c     = 2^(rand() * 16 - 7);
     
     try 
-        error = cross_validation (x_train, y_train, sigma, c);
+        error = cross_validation (x_train_full, y_train_full, sigma, c);
         if (error < min_error) 
            min_error = error;
            best_sigma = sigma;
@@ -88,16 +90,25 @@ end
 %best_c = 2^13;
 
 
-%% training
-% train with best parameter with all training data
-SVMstruct = svmtrain(x_train, y_train, 'Kernel_Function', 'rbf', 'rbf_sigma', best_sigma, 'boxconstraint', best_c);
-
-%% prediction
+%% estimate error with training over subset and testing over subset
 % predict label of unused training_data
-predicted__train_data_label = svmclassify(SVMstruct,x_train_test);
-train_data_error = calc_error (predicted__train_data_label, y_train_test)
+estimate_train_data = training_data(1:1200,:);
+estimate_test_data  = training_data(1201:1447,:);
+estimate_train_x = estimate_train_data(:,1:27);
+estimate_train_y = estimate_train_data(:,28);
+estimate_test_x  = estimate_test_data(:,1:27);
+estimate_test_y  = estimate_test_data(:,28);
+SVMstruct = svmtrain(estimate_train_x, estimate_train_y, 'Kernel_Function', 'rbf', 'rbf_sigma', best_sigma, 'boxconstraint', best_c);
+predicted__train_data_label = svmclassify(SVMstruct, estimate_test_x);
+train_data_error = calc_error (predicted__train_data_label, estimate_test_y)
 calc_grade (train_data_error);
 
+
+%% training
+% train with best parameter with all training data
+SVMstruct = svmtrain(x_train_full, y_train_full, 'Kernel_Function', 'rbf', 'rbf_sigma', best_sigma, 'boxconstraint', best_c);
+
+%% prediction
 % predict label of validation data
 x_validation = csvread('../../data/2/validation.csv');
 predicted_validation_label = svmclassify(SVMstruct, x_validation);
