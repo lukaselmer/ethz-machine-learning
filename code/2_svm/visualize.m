7ecf70a93ea9d6c6e572dffc8c96e720e745347f
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-11-24 16:49:16 +0100

    doc

--- code/2_svm/visualize.m
+++ code/2_svm/visualize.m
@@ -3,15 +3,18 @@ function [  ] = visualize( x, y )
     idx_pos = find (y(:,1) == 1);
     idx_neg = find (y(:,1) == -1);
 
-    positive_x = x (idx_pos,1:end-1);
-    negative_x = x (idx_neg,1:end-1);
+    positive_x = x (idx_pos,:);
+    negative_x = x (idx_neg,:);
 
-    positive_y = y (idx_pos,1:end-1);
-    negative_y = y (idx_neg,1:end-1);
+    %positive_y = y (idx_pos,:);
+    %negative_y = y (idx_neg,:);
 
+    clf;
+    close all;
     hold on;
-    plot (positive_x(:,1), positive_x(:,4), 'g.');
-    plot (negative_x(:,1), negative_x(:,4), 'r.');
+    
+    plot (positive_x(:,1), positive_x(:,7), 'gx');
+    plot (negative_x(:,1), negative_x(:,7), 'r+');
 
 end
 
