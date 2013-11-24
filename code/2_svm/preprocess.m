2f5be148b01b1f584258fa7d781ecab225fd687e
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-24 21:05:03 +0100

    "refactoring"

--- code/2_svm/preprocess.m
+++ code/2_svm/preprocess.m
@@ -2,5 +2,5 @@ function [ Xpre ] = preprocess( X )
     Xpre(:, 1:3:27) = log(5.*log(X(:, 1:3:27)+2.00001) +1.1);
     Xpre(:, 2:3:27) = log(5.*log(X(:, 2:3:27)+2.00001) +1.1);
     Xpre(:, 3:3:27) = log(5.*log(X(:, 3:3:27)+1000.0) +1.1);
-    Xpre = [ones(size(X,1),10000.0) Xpre];
+    %Xpre = [ones(size(X,1),10000.0) Xpre];
 end
