e088463fbb0338e20ce823b05915ee72a3e5c815
Lukas Elmer <lukas.elmer@gmail.com>
2013-12-17 15:34:51 +0100

    best submission

--- code/3_map/preprocessing/external_tools/ML-3/Preprocessor.cs
+++ code/3_map/preprocessing/external_tools/ML-3/Preprocessor.cs
@@ -16,12 +16,20 @@ namespace ML_3
             List<string> filteredTopWords = new List<string>(200);
             foreach (var word in t1000)
             {
-                var replacemnt = filteredTopWords.Find(w => LevenshteinDistance(w, word.Word) <= 1);
+                var replacemnt = filteredTopWords.Find(w => IsReplacement(w, word.Word));
                 if (replacemnt == null)
                     filteredTopWords.Add(word.Word);
             }
 
             return filteredTopWords;
+        }
+
+        private static bool IsReplacement(string w1, string w2)
+        {
+            var len = Math.Max(w1.Length, w2.Length);
+            var levenshteinDistance = LevenshteinDistance(w1, w2);
+            var percentage = (1.0 - (levenshteinDistance / (double)len));
+            return percentage >= 0.8;
         }
 
         internal static IEnumerable<InEntry> Filter(IEnumerable<InEntry> data, List<string> filteredTopWords)
@@ -33,7 +41,7 @@ namespace ML_3
             {
                 item.Text = item.Text.ToLower().Replace("\"", "$");
                 item.Text = item.Text.Split(' ')
-                    .Select(w => filteredTopWords.Find(q => LevenshteinDistance(q, w) <= 1) ?? w) // look if word W can be replaced by a top word, if not return the original word W
+                    .Select(w => filteredTopWords.Find(q => IsReplacement(q, w)) ?? w) // look if word W can be replaced by a top word, if not return the original word W
                     .Aggregate((x, y) => x + " " + y);
                 yield return item;
             }
@@ -46,6 +54,35 @@ namespace ML_3
             //  return data.GroupBy(d => d.Plz).Take(10).SelectMany(g => g);
         }
 
+
+        private static int EditDistance(string original, string modified)
+        {
+            int len_orig = original.Length;
+            int len_diff = modified.Length;
+
+            var matrix = new int[len_orig + 1, len_diff + 1];
+            for (int i = 0; i <= len_orig; i++)
+                matrix[i, 0] = i;
+            for (int j = 0; j <= len_diff; j++)
+                matrix[0, j] = j;
+
+            for (int i = 1; i <= len_orig; i++)
+            {
+                for (int j = 1; j <= len_diff; j++)
+                {
+                    int cost = modified[j - 1] == original[i - 1] ? 0 : 1;
+                    var vals = new int[] {
+                        matrix[i - 1, j] + 1,
+                        matrix[i, j - 1] + 1,
+                        matrix[i - 1, j - 1] + cost
+                    };
+                    matrix[i, j] = vals.Min();
+                    if (i > 1 && j > 1 && original[i - 1] == modified[j - 2] && original[i - 2] == modified[j - 1])
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + cost);
+                }
+            }
+            return matrix[len_orig, len_diff];
+        }
 
         public static int LevenshteinDistance(string s, string t)
         {
